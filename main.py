faeddd52d7d7eba420dccaf0495d41802158d262
Illuminat <miassilya5@gmail.com>
2021-11-17 01:12:53 +0300

    Полностью рабочая

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from tkinter import *
 
+class ColBlock:
+    def __init__(self, master, code, color):
+        self.b = Button(master, text=color, activebackground=code ,bg=code, width=20, command=self.shcolor)
+        self.b.pack()
+        self.code = code
+        self.color = color
+    def shcolor(self):
+        vid["text"] = self.color
+        pole.delete(0, END)
+        pole.insert(0, self.code)
 root = Tk()
 
+vid = Label(root, width=20)
+vid.pack()
+pole = Entry(root, width=20, justify='center')
+pole.pack()
 
 
+red = ColBlock(root, '#ff0000', "красный")
+orange = ColBlock(root, '#ff7d00', "оранжевый")
+yelloy = ColBlock(root, '#ffff00', "жёлтый")
+green = ColBlock(root, '#00ff00', "зелёный")
+blue = ColBlock(root, '#007dff', "голубой")
+dblue = ColBlock(root, '#0000ff', "синий")
+violet = ColBlock(root, '#7d00ff', "фиолетовый")
 
 root.mainloop()
