0a77f39c6645fbe167e9f2f7ec1431815dab2715
Illuminat <miassilya5@gmail.com>
2021-12-14 22:36:40 +0300

    правка

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from tkinter import *
 
+# Добавление в список элементов
 def addList(event):
     lbox.insert(END, entry.get())
     entry.delete(0, END)
 
+# Копирование элемента из списка в поле ввода
 def copyInEntry(event):
     select = list(lbox.curselection())
     select.reverse()
     for i in select:
         entry.insert(0, lbox.get(i))
+        lbox.delete(i)
 
 root = Tk()
 root.title("Notes")
