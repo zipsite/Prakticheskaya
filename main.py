b6bf48079124e4448ddbfc0d765f0c66b6d29f05
Illuminat <miassilya5@gmail.com>
2021-12-14 22:24:27 +0300

    правка

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from tkinter import *
 
+# Функция переключает текст в зависимости от нажатой кнопки
 def clic():
     if rlink.get() == 0:
-        info['text'] = "Реальный абобус"
+        info['text'] = "Вася"
     elif rlink.get() == 1:
         info['text'] = "Просто Петя"
     elif rlink.get() == 2:
-        info['text'] = "Реальная беброчка"
+        info['text'] = "Маша"
+
 
 root = Tk()
-root.title("aboba")
+root.title("Контакты")
 
+# Хранит значение радиокнопки
 rlink = IntVar()
 rlink.set(3)
 
