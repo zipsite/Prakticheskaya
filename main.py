e56ec2e03a02bc9626ed9e97161913bb8d218b1f
Illuminat <miassilya5@gmail.com>
2021-12-14 22:46:50 +0300

    правка

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ c.pack()
 
 c.create_oval(150,10, 180, 40, fill='yellow', outline="yellow")
 c.create_polygon(60,100, 150,100, 150,180, 60,180, fill="lightblue", outline="lightblue")
-c.create_polygon(100,60, 40,100, 170,100, fill="lightblue", outline="lightblues")
+c.create_polygon(100,60, 40,100, 170,100, fill="lightblue", outline="lightblue")
 
 for i in range(0, 250, 5):
     c.create_arc(0+i, 500, 40+i, 150, width=2, start=100, extent=-50, style=ARC, outline="green")
