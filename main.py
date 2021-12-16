0c200a262d424bd87d4441682bc1e9e73f0ec8ff
aboba <bebra@aboba.ru>
2021-12-16 13:20:20 +0300

    Release

--- main.py
+++ main.py
@@ -1,65 +1,24 @@
 from tkinter import *
-from tkinter.messagebox import *
-from tkinter.filedialog import *
+from random import *
 
-# Функции открытия и сохранения файла
-def openfile():
-    global textpad
-    filename = askopenfilename()
-    name = str(filename)
-    file = open(name, "r", encoding='UTF-8')
-    bufer = file.read()
-    text.delete(1.0, END)
-    text.insert(1.0, bufer)
-    file.close()
+def fart(event):
+    xc = randint(0, 1000)
+    yc = randint(0, 1000)
 
-def savefile():
-    global text
-    filename = asksaveasfilename()
-    name = str(filename)
-    file = open(name, "w", encoding='UTF-8')
-    bufer = text.get(1.0, END)
-    file.write(bufer)
-    file.close()
+    rsize = str(xc+100)+"x"+str(yc+100)
 
-def clear():
-    text.delete(1.0, END)
+    root.geometry(rsize)
+    popbtn.place(x=xc, y=yc, width=50, height=50)
 
-
-def spawn(event):
-    popmenu.post(event.x_root, event.y_root)
-
-# Главное окно
 root = Tk()
-root.title("Блохнот")
+root.title("Precol Randomniy")
 
-mainmenu = Menu(root)
-root.config(menu=mainmenu)
+img = PhotoImage(file='smile.png')
 
-mainmenu.add_command(label="Открыть", command=openfile)
-mainmenu.add_command(label="Сохранить", command=savefile)
-
-popmenu = Menu(tearoff=0)
-popmenu.add_command(label='Очистить', command=clear)
-
-# Фрейм текстового поля
-textpad = Frame(root)
-textpad.pack()
-
-# Конфиги текстового поля и скроллбаров
-text = Text(textpad, width = 50, height = 20, wrap=NONE)
-scroll = Scrollbar(textpad, command=text.yview)
-scroll2 = Scrollbar(orient=HORIZONTAL, command=text.xview)
+popbtn = Label(root, image=img)
+popbtn.place(x=75, y=75, width=50, height=50)
 
 
-text.bind('<Button-3>', spawn)
-# Расположение текстового поля и скролл баров
-text.pack(side=LEFT)
-scroll.pack(side=LEFT, fill = Y)
-scroll2.pack(side=BOTTOM, fill=X)
-
-# Подключение скролл баров к текстовому полю
-text.config(yscrollcommand=scroll.set)
-text.config(xscrollcommand=scroll2.set)
+root.bind("<Button-1>", fart)
 
 root.mainloop()
