b5e71c669450a10baa359b90ea4cae3fe1c46f63
aboba <bebra@aboba.ru>
2021-12-16 14:08:38 +0300

    релиз?

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from random import *
 
 def fart(event):
     xc = randint(0, 1000)
-    yc = randint(0, 1000)
+    yc = randint(0, 500)
 
     rsize = str(xc+100)+"x"+str(yc+100)
 
@@ -16,8 +16,7 @@ root.title("Precol Randomniy")
 img = PhotoImage(file='smile.png')
 
 popbtn = Label(root, image=img)
+popbtn.bind("<Button-1>", fart)
 popbtn.place(x=75, y=75, width=50, height=50)
-
-root.bind("<Button-1>", fart)
 
 root.mainloop()
